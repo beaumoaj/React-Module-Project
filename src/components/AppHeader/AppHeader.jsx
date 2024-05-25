96c10df2eb96a2863f85cd75c071c5ebdf3c461f
beaumoaj <a.j.beaumont@aston.ac.uk>
2024-05-25 19:43:34 +0100

    first two features

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,6 +1,9 @@
-// import Logo from "@/assets/spa-logo.png";
-// import "./AppHeader.scss";
-// const AppHeader = () => (
-// what goes in here? there is no content in the AppHeader component
-// );
-// export default AppHeader;
+import Logo from "@/assets/spa-logo.png";
+import "./AppHeader.scss";
+const AppHeader = () => (
+  <header className="app__header">
+    <img src={Logo} alt="Logo" className="app__logo" />
+    <h1 className="app__heading">CYF Hotel</h1>
+  </header>
+);
+export default AppHeader;
