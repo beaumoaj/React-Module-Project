8109de978476f8513c7068ffa8f4f867bfb98e38
beaumoaj <a.j.beaumont@aston.ac.uk>
2024-05-26 10:58:11 +0100

    created a footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,18 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
 import AppHeader from "../AppHeader/AppHeader";
+import Footer from "../Footer/Footer";
+const address = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789",
+];
 
 const App = () => (
   <div className="app">
     <AppHeader />
     <Bookings />
+    <Footer address={address} />
   </div>
 );
 
