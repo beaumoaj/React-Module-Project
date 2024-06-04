2f90ac3ba4cf217526b0a331ffc2f30fca4171ba
beaumoaj <a.j.beaumont@aston.ac.uk>
2024-06-04 14:56:59 +0100

    minor changes to avoid key error

--- src/components/SearchResults/SearchRow.jsx
+++ src/components/SearchResults/SearchRow.jsx
@@ -2,14 +2,14 @@ const SearchRow = ({ result }) => {
   console.log(result);
   return (
     <tr key={"tr" + result.id}>
-      <td key={"id" + result.id}>{result.id}</td>
-      <td key={"title" + result.id}>{result.title}</td>
-      <td key={"first" + result.id}>{result.firstName}</td>
-      <td key={"last" + result.id}>{result.surname}</td>
-      <td key={"email" + result.id}>{result.email}</td>
-      <td key={"roomID" + result.id}>{result.roomId}</td>
-      <td key={"checkin" + result.id}>{result.checkInDate}</td>
-      <td key={"checkout" + result.id}>{result.checkOutDate}</td>
+      <td key={"id_" + result.id}>{result.id}</td>
+      <td key={"title_" + result.id}>{result.title}</td>
+      <td key={"first_" + result.id}>{result.firstName}</td>
+      <td key={"last_" + result.id}>{result.surname}</td>
+      <td key={"email_" + result.id}>{result.email}</td>
+      <td key={"roomID_" + result.id}>{result.roomId}</td>
+      <td key={"checkin_" + result.id}>{result.checkInDate}</td>
+      <td key={"checkout_" + result.id}>{result.checkOutDate}</td>
     </tr>
   );
 };
